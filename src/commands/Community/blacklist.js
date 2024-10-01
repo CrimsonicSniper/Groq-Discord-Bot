244e7d64cb7a8de4642c41bb15fb4aed52d1fa58
Crimson <116349605+CrimsonicSniper@users.noreply.github.com>
2024-09-30 18:48:28 -0700

    Update blacklist.js

--- src/commands/Community/blacklist.js
+++ src/commands/Community/blacklist.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, ButtonBuilder, ActionRowBuilder } = require('discord.js')
+const { SlashCommandBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js')
 const { getGroqChatCompletion } = require('./tools/getGroqChatCompletion')
 const { splitMessage } = require('./tools/splitMessages')
 module.exports = {
